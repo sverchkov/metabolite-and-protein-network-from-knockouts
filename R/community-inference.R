f1f574fb25e53ff148de308187230cddd8762b34
Yuriy Sverchkov <yuriy.sverchkov@gmail.com>
2018-08-05 18:35:38 -0500

    enhanced community inference

--- R/community-inference.R
+++ R/community-inference.R
@@ -13,7 +13,7 @@
 #' @return a data frame with two columns, n - node id (taken from a, b of input) and cluster - unique cluster ID.
 #' @import dplyr
 #' @import futile.logger
-inferCommunities <- function( similarities, save.file = NULL ){
+inferCommunities <- function( similarities, save.file = NULL, full_trajectory = F ){
 
   # Modularity = sum_c (e_cc - a_c^2)
   # where c - communities
@@ -37,7 +37,16 @@ inferCommunities <- function( similarities, save.file = NULL ){
   # IMPLEMENTATION
   
   # Compute merge costs for initial nodes, prepare community indeces
-  nodes <- distinct( similarities, a )
+  membership <- union( distinct( similarities, member = a ), distinct( similarities, member = b ) ) %>%
+    arrange( member )
+  
+  if ( convert_ids <- is.integer( membership$member ) )
+    membership <- mutate( membership, id = member )
+  else
+    membership <- mutate( membership, id = row_number() )
+  
+  membership <- mutate( membership, community = id )
+  next_id <- max( membership$id ) + 1
   
   # Compute total similarity
   total <- summarize( similarities, total = sum( similarity ) )$total
@@ -59,26 +68,44 @@ inferCommunities <- function( similarities, save.file = NULL ){
     rename( deg_a = degree ) %>%
     left_join( degrees, by = c("b" = "id") ) %>%
     rename( deg_b = degree ) %>%
-    transmute( a = as.character( a ), b = as.character( b ),
-               cost = similarity - 2 * deg_a * deg_b / total )
+    transmute( a, b, cost = similarity - 2 * deg_a * deg_b / total )
+  
+  if ( convert_ids )
+    communities <- communities %>%
+      left_join( select( membership, member, a_id = id ), by = c("a" = "member") ) %>%
+      left_join( select( membership, member, b_id = id ), by = c("b" = "member") ) %>%
+      select( a = a_id, b = b_id, cost )
+  
   flog.trace( "Community score change computed." )
   
+  # For full trajectory
+  peak_number <- 1
+  prev_m_cost <- 0
+  
   # The greedy iteration part
-  repeat {
+  while ( nrow( communities ) > 0 ) {
     merge_row <- filter( communities, cost == max( cost ) )
     
-    if ( 1 != ( n_merge <- nrow( merge_row ) ) )
+    if ( 1 != ( n_merge <- nrow( merge_row ) ) ){
       flog.error( "The merge candidate is %s rows :/", merge_row )
-    
-    if ( 0 > ( m_cost <- merge_row$cost ) ){
-      flog.trace("Max merge cost is %s, we're done.", m_cost )
-      break;
+      if ( n_merge < 1 ) stop()
+      if ( n_merge > 1 ) merge_row = sample_n( merge_row, 1 )
     }
+    
+    if ( 0 > ( m_cost <- merge_row$cost ) && 0 <= prev_m_cost ){
+      if ( full_trajectory ) {
+        membership[[paste("Peak", peak_number)]] <- membership$community
+        peak_number <- 1 + peak_number
+      } else {
+        flog.trace("Max merge cost is %s, we're done.", m_cost )
+        break;
+      }
+    }
+    prev_m_cost <- m_cost
     
     m_ids <- merge_row[ c( "a", "b" ) ]
     flog.trace( "Merging %s with %s...", merge_row$a, merge_row$b )
-    new_id <- paste( m_ids, collapse = " " )
-    
+
     # Find all rows where selected communities appear
     find_communities <- mutate( communities, target = ( a %in% m_ids ) || ( b %in% m_ids ) )
     # Will remove them from the community table
@@ -89,20 +116,22 @@ inferCommunities <- function( similarities, save.file = NULL ){
                  communities %>% filter( b %in% m_ids, !( a %in% m_ids ) ) %>% rename( src = a, tgt = b ) ) %>%
       group_by( src ) %>%
       summarize( new_cost = sum( cost ) ) %>%
-      transmute( a = src, b = new_id, cost = new_cost )
+      transmute( a = src, b = next_id, cost = new_cost )
     
     # Add to community table
     communities <- union_all( filter( communities, !( a %in% m_ids ), !( b %in% m_ids ) ), new_community_costs )
-    
+
     flog.trace("...merged!")
+
+    # Update membership table
+    membership <- mutate( membership, community = if_else( community %in% m_ids, as.integer( next_id ), community ) )
+    
+    # Update next id
+    next_id <- 1 + next_id
     
     if( !is.null( save.file ) )
       saveRDS( communities, save.file )
   }
   
-  return ( communities )
-}
-
-computeModularity <- function( communities ){
-  
+  return ( list( communities = communities, membership = membership ) )
 }
