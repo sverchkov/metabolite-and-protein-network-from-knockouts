2e25f0faad01fdfbd5ddc4223a3eb7cec095d914
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-10-29 16:01:46 -0500

    added strings mapping file download

--- scripts/download-bgk.sh
+++ scripts/download-bgk.sh
@@ -9,3 +9,6 @@ echo "Created by scripts/download-bgk.sh" > README
 
 curl https://stringdb-static.org/download/protein.links.v10.5.txt.gz -o protein.links.v10.5.txt.gz
 gunzip protein.links.v10.5.txt.gz
+
+curl https://string-db.org/mapping_files/uniprot_mappings/full_uniprot_2_string.04_2015.tsv.gz -o full_uniprot_2_string.04_2015.tsv.gz
+gunzip full_uniprot_2_string.04_2015.tsv.gz
