ef0808acd59db65a2faebbcedbb9e44f736b9b60
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2018-12-14 16:56:28 -0600

    Network fusion progress

--- R/fuse-networks.R
+++ R/fuse-networks.R
@@ -25,11 +25,36 @@ ko_protein_links <-
 
 # Put all edges in one DF
 cy_edges <- union_all( annotated_molecule_edges, fgnem_ko_edges ) %>%
-  union_all( ko_protein_links )
+  union_all( ko_protein_links ) %>%
+  as.data.frame()
 
 # Make nodes df
-cy_nodes <- molecule_ids %>% filter( `Molecule ID` %in% nodes ) %>%
+molecule_nodes <- union(
+  distinct( annotated_molecule_edges, `Molecule ID` = source ),
+  distinct( annotated_molecule_edges, `Molecule ID` = target ) )
+
+cy_nodes <- molecule_nodes %>% left_joiin( molecule_ids ) %>%
   select( -id ) %>%
   select( id = `Molecule ID`, full_name = `Molecule Name`, type = `Molecule Type` ) %>%
   union( tibble( id = kos, full_name = id, type = `Knockout` ) ) %>%
   as.data.frame()
+
+# Save objects
+destination <- "results/fused-network"
+dir.create( destination, recursive = T )
+
+saveRDS( cy_edges, paste0( destination, "edges.rds" ) )
+saveRDS( cy_nodes, paste0( destination, "nodes.rds" ) )
+
+# Run Cytoscape
+if ( askYesNo( "Make Cytoscape network?" ) ){
+  # Create cytoscape network with RCy3
+  net_id <- RCy3::createNetworkFromDataFrames(
+    nodes = cy_nodes,
+    edges = cy_edges,
+    title = paste( "Annotated Cosine Similarity Network", date() ),
+    collection = "H3K Networks" )
+  
+  # Visual style
+  # TODO
+}
